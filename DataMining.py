fcc948c0f84623cdceef0e5de488abd21a153b3c
harryl6798 <harryl6798@gmail.com>
2019-11-14 22:57:21 -0600

    Wow this is harder than I thought

--- DataMining.py
+++ DataMining.py
@@ -1,7 +1,9 @@
 #DataMining Project
 import sys
+# from collections import OrderedDict
 
 #Define default variables
+MLEM2_order = []
 attributes = []
 data_input = []
 num_attributes = 0
@@ -35,13 +37,81 @@ def importData(file_name):
         editLine = line.split()
         data_input.append(editLine)
 
-
+    
     return file_name
 
-def MLEM2(attributes, data_input):
-    return attributes
+#Prepping the data for the input 
+def data_prep(attributes, data_input, num_attributes, num_row, MLEM2_order):
+
+    #Get Data Per Attribute
+    data_per_attribute = []
+    for i in range(num_attributes):
+        newList = []
+        for x in range(num_row):
+            newList.append(data_input[x][i])
+        data_per_attribute.append(newList)
+
+    list_of_dictionaries = []
+    
+    for newList in data_per_attribute:
+        unique = [x for i, x in enumerate(newList) if i == newList.index(x)]
+        MLEM2_order.append(unique)
+        unique_list = set(newList)
+        #print(unique_list)
+        unique_list = list(unique_list)
+        dictionary_perList = {}
+        for val in unique_list:
+            list_of_indexes= [i for i, e in enumerate(newList) if e == val]
+            dictionary_perList[val] = list_of_indexes
+        
+        list_of_dictionaries.append(dictionary_perList)
+    #print(list_of_dictionaries)
+
+    return list_of_dictionaries
+
+#Perform the MLEM 2 Algo
+def MLEM2(list_dic, attributes, data_input, num_attributes, num_row, MLEM2_order):
+
+    decision = list_dic[-1]
+    decision_list = MLEM2_order[-1]
+    #print(decision_list)
+
+    rules =[]
+    
+    # i = the decision attribute we are doing
+    for i in decision_list:
+        CleanMLEM2 = MLEM2_order
+        elements = decision[i]
+        finalList = []
+        while set(elements) != set(finalList):
+            similarValues = ""
+            numberOfMatchingRows = 0
+            
+            #Goes through the list of attributes and checks
+            for x in range(num_attributes-1):
+                ElementList = CleanMLEM2[x]
+                Dictionary_Column = list_dic[x]
+                for key in ElementList:
+                    dictionary_value = Dictionary_Column[key]
+                    check_if_larger = list(set(dictionary_value).intersection(set(elements)))
 
 
+                    #Checks if the return is empty (If so... stop considering it)
+                    if not check_if_larger:
+                        CleanMLEM2[x].remove(key)
+
+                    if numberOfMatchingRows <= len(check_if_larger):
+
+
+                    
+                    # print(key)
+                    # print(dictionary_value)
+            #print(MLEM2_order)        
+            break
+
+
+
+    return
 
 
 
@@ -54,6 +124,12 @@ print("What is the file you want to output to?")
 outputFile = input()
 num_attributes = len(attributes)
 num_row = len(data_input)
+dictionaries = data_prep(attributes, data_input, num_attributes, num_row, MLEM2_order)
+finalRules = MLEM2(dictionaries, attributes, data_input, num_attributes, num_row,MLEM2_order)
+
+
+
+
 
 
 
